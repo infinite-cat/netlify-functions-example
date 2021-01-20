3f9e72734c3e3ed4f9f6927676dc0702a18797f9
Eugene Draitsev <ddrrai@gmail.com>
2021-01-20 23:41:51 +0100

    Added recap client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-exports.handler = async (event, context) => {
+import { wrapLambdaHandler } from '@recap.dev/client'
+
+export const handler = wrapLambdaHandler(async (event, context) => {
   return {
     statusCode: 200,
     body: "Hello, World"
   };
-};
+});
